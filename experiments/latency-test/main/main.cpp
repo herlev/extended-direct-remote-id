71f06968ea5d2577c4daa2ec013ba46527c9ea79
MadsHoeffer <mads.fogh2@gmail.com>
2024-04-03 10:21:36 +0200

    latency test comments in main.cpp

--- experiments/latency-test/main/main.cpp
+++ experiments/latency-test/main/main.cpp
@@ -1,3 +1,5 @@
+// Define Beacon TX to make this device broadcast a ODID package with increasing message counter. Frequency of 10Hz
+// If not defined the device will act as a relaying device
 #define BeaconTX
 #ifdef BeaconTX
 #include "beacon-tx.hpp"
